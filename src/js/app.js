bc04d6e63e66d6dffd4ddb90a29d1b99c60b98a1
Anthony Ribeiro <anthony.ribeiro77@gmail.com>
2021-01-07 18:34:20 +0100

    blabla.

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,25 @@
-console.log('ok go');
+import Phaser from 'phaser';
+
+class Game extends Phaser.Game {
+
+    constructor() {
+
+        super({
+            type: Phaser.AUTO,
+            width: 800,
+            height: 600,
+            physics: {
+                default: 'arcade',
+                arcade: {
+                    gravity: { y: 200 }
+                }
+            },
+            scene: {
+                preload: preload,
+                create: create
+            }
+        });
+    }
+}
+
+window.game = new Game;
